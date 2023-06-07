6dbdd9e74e910808fb9dfd33463bada303994574
kameradregno <regon0705@gmail.com>
2023-06-07 21:01:15 +0700

    p

--- database/migrations/2023_06_07_140038_create_posts_table.php
+++ database/migrations/2023_06_07_140038_create_posts_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('email');
-            $table->string('username');
-            $table->string('password');
-            $table->enum('role', ['USER', 'ADMIN'])->default(['USER']);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
