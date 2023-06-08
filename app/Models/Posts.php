336af3ae0d18477af0ae7712cba132a316931715
kameradregno <regon0705@gmail.com>
2023-06-08 20:58:22 +0700

    tes

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Posts extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'id', 'user_id', 'title', 'slug', 'content', 'image'
+    ];
+
+    /**
+     * Get the user that owns the Posts
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
