b5fbaccce0be098047ee1db67487ab66ec8a2091
afaizulh <ijulhakim14@gmail.com>
2023-06-08 20:20:35 +0700

    post table

--- database/migrations/2023_06_07_140038_create_posts_table.php
+++ database/migrations/2023_06_07_140038_create_posts_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('content');
+            $table->string('image')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
