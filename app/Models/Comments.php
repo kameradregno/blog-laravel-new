1ad8e31d48e4ea86f2161d374ed9fbc47c95a72e
afaizulh <ijulhakim14@gmail.com>
2023-06-08 21:15:15 +0700

    update

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comments extends Model
 {
@@ -11,5 +13,15 @@ class Comments extends Model
 
     protected $fillable = [
         'user_id', 'post_id', 'content'
-    ]
+    ];
+
+    /**
+     * Get the user associated with the Comments
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
