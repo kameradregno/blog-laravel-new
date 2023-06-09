7d9ef0430d66d6957c76d16bd9efb46fa39945ec
kameradregno <regon0705@gmail.com>
2023-06-09 21:57:01 +0700

    tes

--- resources/views/layouts/auth/register.blade.php
+++ resources/views/layouts/auth/register.blade.php
@@ -8,11 +8,7 @@
         <div class="card-header">Daftar</div>
         <div class="card-body">
             <form method="POST" action="{{ route('register') }}">
-            <div class="mb-3">
-                <label for="Name"></label>
-                <input type="text" name="name" id="name">
-            </div>
-
+            @csrf
             <div class="mb-3">
                 <label for="name" class="form-label">Nama</label>
                 <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
