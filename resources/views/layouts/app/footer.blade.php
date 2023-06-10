b9f71740fb373e9797e8e9cd6743a01caded511d
kameradregno <regon0705@gmail.com>
2023-06-10 20:27:01 +0700

    tes

--- resources/views/layouts/app/footer.blade.php
+++ resources/views/layouts/app/footer.blade.php
@@ -1,26 +1,43 @@
-
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top fixed-bottom">
         <div class="col-md-4 d-flex align-items-center">
             <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
                 <svg class="bi" width="30" height="24">
                     <use xlink:href="#bootstrap"></use>
                 </svg>
             </a>
-            <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+            <span class="mb-3 mb-md-0 text-body-secondary">© 2023 News-ku, Corp</span>
         </div>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24"
-                        height="24">
-                        <use xlink:href="#twitter"></use>
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex me-5">
+            <li class="ms-3"><a class="text-body-secondary" href="#"><svg width="24" height="24"
+                        viewBox="0 0 48 48" id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <style>
+                                .cls-1 {
+                                    fill: none;
+                                    stroke: #000000;
+                                    stroke-linecap: round;
+                                    stroke-linejoin: round;
+                                }
+                            </style>
+                        </defs>
+                        <path class="cls-1"
+                            d="M35.38,10.46a2.19,2.19,0,1,0,2.16,2.22v-.06A2.18,2.18,0,0,0,35.38,10.46Z" />
+                        <path class="cls-1"
+                            d="M40.55,5.5H7.45a2,2,0,0,0-1.95,2v33.1a2,2,0,0,0,2,2h33.1a2,2,0,0,0,2-2V7.45A2,2,0,0,0,40.55,5.5Z" />
+                        <path class="cls-1" d="M24,15.72A8.28,8.28,0,1,0,32.28,24h0A8.28,8.28,0,0,0,24,15.72Z" />
                     </svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24"
-                        height="24">
-                        <use xlink:href="#instagram"></use>
+
+            <li class="ms-3"><a class="text-body-secondary" href="#"><svg width="24" height="24"
+                        viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#000000">
+                        <path d="M28 56V20Q28 8 40 8" />
+                        <line x1="40" y1="24" x2="22" y2="24" />
                     </svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24"
-                        height="24">
-                        <use xlink:href="#facebook"></use>
+
+            <li class="ms-3"><a class="text-body-secondary" href="#"><svg width="24" height="24"
+                        viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#000000">
+                        <line x1="40" y1="28" x2="27.88" y2="41.92" />
+                        <polygon points="48 56 56 8 8 28 48 56" />
                     </svg></a></li>
         </ul>
     </footer>
--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -1,11 +1,11 @@
-<div class="container">
     <header
         class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <div class="col-md-3 mb-2 mb-md-0">
             <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-                <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
-                    <use xlink:href="#bootstrap"></use>
+                <svg class="bi" width="40" height="32" role="img" aria-label="newspaper">
+                    <path d="M0 26.016q0 2.496 1.76 4.224t4.256 1.76h20q2.464 0 4.224-1.76t1.76-4.224v-22.016h-4v-1.984h-4v-2.016h-24v26.016zM4 26.016v-22.016h16v24h-13.984q-0.832 0-1.44-0.576t-0.576-1.408zM6.016 26.016h5.984v-2.016h-5.984v2.016zM6.016 22.016h8v-2.016h-8v2.016zM6.016 18.016h5.984v-2.016h-5.984v2.016zM6.016 14.016h12v-8h-12v8zM14.016 18.016h4v-2.016h-4v2.016zM16 22.016h2.016v-2.016h-2.016v2.016zM24 25.024v-21.024h2.016v21.024q0 0.384 0.288 0.704t0.704 0.288 0.704-0.288 0.288-0.704v-19.008h2.016v19.008q0 1.248-0.896 2.112t-2.112 0.864-2.144-0.864-0.864-2.112z"></path>
                 </svg>
+                <h3 class="ms-2">News-ku</h3>
             </a>
         </div>
 
@@ -31,4 +31,3 @@
         </div>
 </div>
 </header>
-</div>
