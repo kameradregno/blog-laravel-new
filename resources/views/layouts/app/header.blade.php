80a2ac64b1fe48966d0d51b3a2e2582cb71867a9
kameradregno <regon0705@gmail.com>
2023-06-10 21:27:02 +0700

    tes

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -15,11 +15,16 @@
     </div>
 
     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        @guest
         <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
         <li><a href="#" class="nav-link px-2">Features</a></li>
         <li><a href="#" class="nav-link px-2">Pricing</a></li>
         <li><a href="#" class="nav-link px-2">FAQs</a></li>
-        <li><a href="#" class="nav-link px-2">About</a></li>
+        <li><a href="#" class="nav-link px-2">About</a></li> 
+        @else
+        <li><span class="text-dark px-2"><b>Selamat datang,</b> <i>{{ Auth::user()->username }}</i></span></li>      
+        @endguest
+        
     </ul>
 
     <div class="col-md-3 text-end">
