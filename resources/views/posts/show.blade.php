60fb547428808eaea0ee8eadaf3b1adc6367a40b
kameradregno <regon0705@gmail.com>
2023-06-10 20:05:46 +0700

    tes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
-@section('title')
+@section('title', $post->title)
 
 @section('content')
 
 <article class="blog-post mt-5">
-    <h2 class="blog-post-title mb-1">{{$p->title}}</h2>
-    <p class="blog-post-meta">{{ date('d M Y H:i', strtotime($p->created_at)) }}</p>
+    <h2 class="blog-post-title mb-1">{{$post->title}}</h2>
+    <p class="blog-post-meta">{{ date('d M Y H:i', strtotime($post->created_at)) }}</p>
 
-    <p>{{$p->content}}</p>
+    <p>{{$post->content}}</p>
 </article>
 
 <a href="{{ route('posts') }}"><-</a>
