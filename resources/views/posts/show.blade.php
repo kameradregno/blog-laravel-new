9765362e4be198e711832a29cf5085de3da930d7
farrellarrafiifris <jshzhbsj@gmail.com>
2023-06-10 20:18:34 +0700

    yes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,7 +11,48 @@
     <p>{{$post->content}}</p>
 </article>
 
-<a href="{{ route('posts') }}"><-</a>
+<p class="text-muted">{{ $total_comments }} Komentar</p>
+
+@foreach ($comments->take(1) as $comment)
+        <div class="card mb-3">
+            <div class="card-header text-primary">
+                &#64;{{ $comment->commentwriter->name }}
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>{{ $comment->comment }}</p>
+                    <p class="blockquote-footer"><small>Commented At :
+                            {{ date('d M Y H:i', strtotime($comment->created_at)) }}</small></p>
+                </blockquote>
+            </div>
+        </div>
+    @endforeach
+
+    @if ($comments->count() > 1)
+
+        <div class="position-relative">
+            <a class="position-absolute top-50 start-50 translate-middle" id="showAllComments">Show All Comments</a>
+        </div>
+
+        <div id="hiddenComments" style="display: none;">
+            @foreach ($comments->skip(1) as $comment)
+                <div class="card mb-3">
+                    <div class="card-header text-primary">
+                        &#64;{{ $comment->commentwriter->name }}
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>{{ $comment->comment }}</p>
+                            <p class="blockquote-footer"><small>Commented At :
+                                    {{ date('d M Y H:i', strtotime($comment->created_at)) }}</small></p>
+                        </blockquote>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+<a href="{{ route('posts') }}">Back</a>
+
 
 
 @endsection
