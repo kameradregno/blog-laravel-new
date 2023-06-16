71621518f7ee1fd471adf9b7027c8b9ac249b42c
kameradregno <regon0705@gmail.com>
2023-06-16 20:44:37 +0700

    p

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             @foreach ($post as $p)
                 <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
@@ -16,12 +16,12 @@
                 </div>
             @endforeach
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
             data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
             data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
