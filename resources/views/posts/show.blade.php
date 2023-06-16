fc805f53d4d87cdbce63ec3a9ce0515eecde4c9d
afaizulh <ijulhakim14@gmail.com>
2023-06-16 09:18:30 +0700

    update show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,55 +17,67 @@
         <p class="blog-post-meta">Created at : {{ date('d M Y', strtotime($data->created_at)) }}</p>
     </article>
 
-    <form action="{{ route('comment') }}" class="d-flex my-5" method="POST">
-        @csrf
-        <div class="input-group">
-            <input type="text" class="form-control" name="comment" placeholder="Buat Komentar">
-            <input type="hidden" name="post_id" value="{{ $data->id }}">
-            <button type="submit" class="btn btn-outline-secondary">Kirim</button>
-        </div>
-    </form>
-
     <div class="card mb-3">
         <div class="card-header">
             {{ $total_comments }} Komentar
         </div>
-        <div class="card-body">
-            <div class="container">
-                @foreach ($comments->take(1) as $comment)
-                    <div class="my-1">
-                        <p class="text-muted mb-1">&#64;{{ $comment->commentwriter->username }}</p>
-                        <blockquote class="blockquote mb-0">
-                            <p>{{ $comment->comment }}</p>
-                            <p class="blockquote-footer"><small>Commented At :
-                                    {{ date('d M Y H:i', strtotime($comment->created_at)) }}</small></p>
-                        </blockquote>
-                    </div>
-                @endforeach
-
-                @if ($comments->count() > 1)
-
-                    <div class="position-relative my-4">
-                        <a class="position-absolute top-50 start-50 translate-middle" id="showAllComments">Show All
-                            Comments</a>
-                    </div>
-
-                    <div id="hiddenComments" style="display: none;">
-                        @foreach ($comments->skip(1) as $comment)
-                            <div class="my-4">
-                                <p class="text-muted mb-1">&#64;{{ $comment->commentwriter->username }}</p>
-                                <blockquote class="blockquote mb-0">
-                                    <p>{{ $comment->comment }}</p>
-                                    <p class="blockquote-footer"><small>Commented At :
-                                            {{ date('d M Y H:i', strtotime($comment->created_at)) }}</small></p>
-                                </blockquote>
-                            </div>
-                    </div>
+        <form action="{{ route('comment') }}" class="d-flex m-2" method="POST">
+            @csrf
+            <div class="input-group">
+                <input type="text" class="form-control" name="comment" placeholder="Buat Komentar">
+                <input type="hidden" name="post_id" value="{{ $data->id }}">
+                <button type="submit" class="btn btn-outline-secondary">Kirim</button>
             </div>
-            @endforeach
-        </div>
+        </form>
     </div>
-    
+
+    <div class="container">
+        @foreach ($comments->take(1) as $comment)
+            <div class="my-1">
+                <h6 class="fw-bold mb-1">&#64;{{ $comment->commentwriter->username }}</h6>
+                <blockquote class="blockquote mb-0">
+                    <p>
+                        <small>
+                            {{ $comment->comment }}
+                        </small>
+                    </p>
+                    <p class="blockquote-footer mt-2">
+                        <small>
+                            Commented at {{ date('d M Y H:i', strtotime($comment->created_at)) }}
+                        </small>
+                    </p>
+                </blockquote>
+            </div>
+        @endforeach
+
+        @if ($comments->count() > 1)
+
+            <div class="position-relative my-4">
+                <a class="position-absolute top-50 start-50 translate-middle" id="showAllComments">
+                    Show All Comments
+                </a>
+            </div>
+
+            <div id="hiddenComments" style="display: none;">
+                @foreach ($comments->skip(1) as $comment)
+                <div class="my-4">
+                    <h6 class="fw-bold mb-1">&#64;{{ $comment->commentwriter->username }}</h6>
+                    <blockquote class="blockquote mb-0">
+                        <p>
+                            <small>
+                                {{ $comment->comment }}
+                            </small>
+                        </p>
+                        <p class="blockquote-footer mt-2">
+                            <small>
+                                Commented at {{ date('d M Y H:i', strtotime($comment->created_at)) }}
+                            </small>
+                        </p>
+                    </blockquote>
+                </div>
+                @endforeach
+            </div>
+    </div>
 
     <script>
         document.getElementById("showAllComments").addEventListener("click", function() {
