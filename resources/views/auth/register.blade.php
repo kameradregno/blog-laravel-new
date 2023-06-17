aac85e3e362bef87882eb553b1feb71343bb7179
kameradregno <regon0705@gmail.com>
2023-06-17 13:35:57 +0700

    changed login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,43 +6,48 @@
 
 
 
-@if (session()->has('error_message'))
-    <div class="alert alert-danger">
-        {{session()->get('error_message')}}
-    </div>
-@endif
-    
-    <div class="card">
-        <div class="card-header">Daftar</div>
-        <div class="card-body">
-            <form method="POST" action="{{ route('register') }}">
-            @csrf
-            
-            <div class="mb-3">
-                <label for="username" class="form-label">Nama</label>
-                <input type="text" class="form-control" name="username" id="username" value="{{ old('username') }}">
-            </div>
-            
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email" id="email" required>
-            </div>
-
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Kata Sandi</label>
-                <input type="password" class="form-control" name="password" id="password" required>
-            </div>
-            
-            <div class="mb-3">
-                <label for="password_confirmation" class="form-label">Konfirmasi Kata Sandi</label>
-                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation">
-            </div>
-
-            <button type="submit" class="btn btn-primary">Daftar</button>
-            
-            </form>
-
+    @if (session()->has('error_message'))
+        <div class="alert alert-danger">
+            {{ session()->get('error_message') }}
         </div>
-    </div>   
+    @endif
+
+    <div class="col-md-4 mx-auto">
+
+        <div class="card">
+            <div class="card-header">Daftar</div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Nama</label>
+                        <input type="text" class="form-control" name="username" id="username"
+                            value="{{ old('username') }}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Kata Sandi</label>
+                        <input type="password" class="form-control" name="password" id="password" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Konfirmasi Kata Sandi</label>
+                        <input type="password" class="form-control" name="password_confirmation" id="password_confirmation">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Daftar</button>
+
+                </form>
+
+            </div>
+        </div>
+
+    </div>
 
 @endsection
