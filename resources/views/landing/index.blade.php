64a5e2cf6e21b7fd9c6e99e08b46102ab2c2d940
kameradregno <regon0705@gmail.com>
2023-06-17 08:33:05 +0700

    tess

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -4,25 +4,26 @@
 
 @section('content')
 
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             @foreach ($post as $p)
                 <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
-                    <img src="{{ asset('storage/' . $p->image) }}" class="d-block w-100 h-auto rounded-2 opacity-50" alt="Slide Image">
+                    <img src="{{ asset('storage/' . $p->image) }}" class="d-block w-100 h-auto rounded-2 opacity-50"
+                        alt="Slide Image">
                     <div class="carousel-caption d-none d-md-block text-dark">
                         <h3>{{ $p->title }}</h3>
-                        <p>{!! substr($p->content, 0, 55) !!}</p>
+                        <a href="{{ url("posts/$p->slug") }}" class="text-dark">
+                            <p class="text-dark">Selengkapnya....</p>
+                        </a>
                     </div>
                 </div>
             @endforeach
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
-            data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
-            data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
@@ -44,8 +45,8 @@
                     </small>
                 </p>
 
-                <a href="{{ url("landing/$p->slug") }}" class="btn btn-success">Selengkapnya</a>
-                
+                <a href="{{ url("posts/$p->slug") }}" class="btn btn-success">Selengkapnya</a>
+
             </div>
         </div>
     @endforeach
